be2c0146845ef0319ae0d86bfb72781797bbedeb
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-16 21:14:56 +0530

    user model done

--- model/user.models.js
+++ model/user.models.js
@@ -1,1 +1,22 @@
 const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+    name:{type : String, require:true},
+    email:{type :String, require:true},
+    password:{type: String,require:true},
+    city:{type: String,require:true},
+    age:{type: Number, require:true},
+    role:{type: String,
+      enum:["admin","librarian","reader"],
+      default:"reader"
+    }
+},{
+    versionKey:false
+})
+
+const UserModel = mongoose.model("user",userSchema);
+
+module.exports={
+    UserModel,
+}
