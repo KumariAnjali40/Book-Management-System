cbdc211a26838a2cfb8ce59dfb268edc176ced28
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-16 21:07:42 +0530

    basic route

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const {connection}=require('./db');
+
+const app = express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+  res.send("heelo");
+})
+
+
+app.listen(4500,async()=>{
+try{
+  await connection
+  console.log("connected to db");
+  console.log("Server is running at port 4500");
+}catch(err){
+    console.log(err);
+}
+})
