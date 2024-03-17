a00f778b2147e593117ec1f19bd131a24187c80c
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-17 18:49:23 +0530

    graphql completed

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-const express = require('express');
-const { connection } = require('./db');
-const { userRouter } = require('./routes/user.routes');
-const { bookRouter } = require('./routes/book.routes');
+const express = require("express");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/user.routes");
+const { bookRouter } = require("./routes/book.routes");
 
-const { graphqlHTTP } = require('express-graphql');
-const { readFileSync } = require('fs');
-const { join } = require('path');
-const { makeExecutableSchema } = require('@graphql-tools/schema');
-const resolvers = require('./resolvers');
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const resolvers = require("./resolvers");
 
 // Read GraphQL schema from file
-const schemaString = readFileSync(join(__dirname, 'schema', 'schema.graphql'), 'utf8');
+const schemaString = readFileSync(
+  join(__dirname, "schema", "schema.graphql"),
+  "utf8"
+);
 const schema = makeExecutableSchema({ typeDefs: schemaString, resolvers });
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/user', userRouter);
-app.use('/book', bookRouter);
+app.use("/user", userRouter);
+app.use("/book", bookRouter);
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: resolvers,
-  graphiql: true 
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({ msg: "Welcome" });
+});
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "OOps! page does not exist" });
 });
 
 app.listen(4500, async () => {
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,37 +1,34 @@
-const jwt = require('jsonwebtoken');
-const {UserModel}=require('../models/user.model');
-const {BlackListModel}=require('../models/blacklist.model');
-const {BookModel}=require('../models/book.model');
+const jwt = require("jsonwebtoken");
+const { UserModel } = require("../models/user.model");
+const { BlackListModel } = require("../models/blacklist.model");
+const { BookModel } = require("../models/book.model");
 
-const auth=async(req,res,next)=>{
-    const access_token=req.headers.authorization?.split(" ")[1];
-    console.log(access_token);
-    if(await BlackListModel.findOne({access_token})){
-        return res.json({msg:"You have been logged out"});
+const auth = async (req, res, next) => {
+  const access_token = req.headers.authorization?.split(" ")[1];
+  //   console.log(access_token);
+  if (await BlackListModel.findOne({ access_token })) {
+    return res.json({ msg: "You have been logged out" });
+  }
+  if (access_token) {
+    try {
+      const decoded = jwt.verify(access_token, "Anjali");
+      //   console.log(decoded);
+      const { userID } = decoded;
+      const user = await UserModel.findOne({ _id: userID });
+      const require_role = user?.role;
+      req.userID = userID;
+      req.role = require_role;
+
+      next();
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ msg: err });
     }
-    if(access_token){
-        try{
-            const decoded=jwt.verify(access_token,"Anjali");
-            console.log(decoded);
-            const {userID}=decoded;
-            const user=await UserModel.findOne({_id:userID});
-            const require_role=user?.role;
-            req.userID=userID;
-            req.role=require_role;
+  } else {
+    res.json({ msg: "Please Login" });
+  }
+};
 
-            next();
-
-        }
-        catch(err){
-            console.log(err);
-            res.status(400).json({msg:err});
-        }
-    }else{
-        res.json({msg:"Please Login"});
-    }
-}
-
-
-module.exports={
-    auth,
-}
+module.exports = {
+  auth,
+};
